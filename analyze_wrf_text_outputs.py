4530e57cf36f4b8b29cd3bb3e37b4ddbb8d259c0
Eric Allen <allenea@udel.edu>
2020-03-24 16:59:33 -0400

    Update analyze_wrf_text_outputs.py

--- analyze_wrf_text_outputs.py
+++ analyze_wrf_text_outputs.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 """
 Created on Tue Aug 28 15:23:32 2018
@@ -12,88 +12,104 @@ Analyzes:
     OBSGRID OUTPUTS
     WRF OUTPUTS
 """
-
-            
-#%% IMPORTS
-import pandas as pd   # Numpy doesn't need an excuse to be used
-import glob
+##IMPORTS
 import os
+#import sys
+from datetime import datetime
+import glob
+import pandas as pd
 
 # =============================================================================
-
 ### NAMING STRUCTURE THAT POINTS TO THE DIRECTORY or DIRECTORIES
-dates = ["M-D-YYYY", "M-D-YYYY","M-D-YYYY"]
-independent_variables = ["VAR1","VAR2","VAR3","VAR4"] # DATA ASSIMILATION TYPES
+DATES = ['YYYY-MM-DD_HH:MM']
 
-#models = ["WRF","COAWST"]
+INDEPENDENT_VARIABLES = ["RUN_NAME"]
 
-model_data_dir = os.path.abspath('../Model_Outputs/')
-for date in dates:
+MODEL_DATA_DIR = os.path.abspath('../../SAVE_OUTPUTS/')
+
+
+
+for date in DATES:
     #for model in models
-    for type1 in independent_variables:
-        mydir = model_data_dir+'/CaseStudy_' + date + '/'+type1 +'_' + date.replace('-','_') + '/'
-        
-        
+    for type1 in INDEPENDENT_VARIABLES:
+
+        prefix_casestudy = "CaseStudy_"
+        dtuple = datetime.strptime(date, "%Y-%m-%d_%H:%M")
+        short_time = dtuple.strftime('%-m-%-d-%Y')
+        mydir = MODEL_DATA_DIR+'/'+prefix_casestudy+ short_time + '/'+type1 +\
+                                    '_' + short_time.replace('-', '_') + '/'
+
 # =============================================================================
-        ### ABOVE: POINT TO THE DIRECTORY... 
-        print ("----------------------START OF Folder--------------------------")
-        print (mydir)
-        smallSize= len(mydir)
-        for file in glob.glob(mydir+'*'):
+        ### ABOVE: POINT TO THE DIRECTORY...
+        print("----------------------START OF Folder--------------------------")
+        print(mydir)
+        smallSize = len(mydir)
+        filelist = glob.glob(mydir+'*')
+        filelist.sort()
+
+        #Case Numbers
+        num_errors = 0
+        num_warnings = 0
+        num_tbounds = 0
+        num_cfl = 0
+
+        for file in filelist:
             shortfile = file[smallSize:]
-            
-            ## OBSGRID TEXT FILES   
+
+            ## OBSGRID TEXT FILES
             if "obsgrid" in shortfile:
-                infile = open(file,'rU')
+                infile = open(file, 'r')
                 readData = infile.readlines()
                 buddyCount = 0
                 errmxCount = 0
                 countQC = 0
-                
+
                 countEmpty = 0
                 countOutside = 0
                 countIngested = 0
-                
+
                 countReported = 0
                 countDuplicates = 0
                 countMerged = 0
-                print (shortfile)
+
+                #print (shortfile)
                 for row in readData:
                     row = row.rstrip("\n")
                     if "BUDDY" in row:
-                        buddyCount +=1
+                        buddyCount += 1
                         if buddyCount > 9:
-                            countQC +=1
+                            countQC += 1
                             #print date, "\t",  type1,"\t", buddyCount - 9, "Buddy Counts"
                     if "ERRMX" in row:
-                        countQC +=1
-                        errmxCount +=1
+                        countQC += 1
+                        errmxCount += 1
                         #print date, "\t",  type1,"\t", errmxCount , "ERRMX Counts"
-                    
+
                     if "Number of observations successfully ingested:" in row:
                         tmp = row.split(":")[1]
                         valINGEST = int(tmp.rstrip("."))
                         countIngested += valINGEST
                         #print "Ingested:  ",valINGEST, "  . Total:  ", countIngested
-                        
+
                     if "Number of empty observations discarded:" in row:
-                        tmp =row.split(":")[1]
-                        numEmpty =  int(tmp.rstrip("."))
+                        tmp = row.split(":")[1]
+                        numEmpty = int(tmp.rstrip("."))
                         if numEmpty != 0:
                             countEmpty += numEmpty
-                            print ("EMPTY!!!:  ", numEmpty)
-                            
+                            print("EMPTY!!!:  ", numEmpty)
+
                     if "Number of observations discarded outside of domain:" in row:
-                        tmp =row.split(":")[1]
+                        tmp = row.split(":")[1]
                         numDiscard = int(tmp.rstrip("."))
                         if numDiscard != 0:
-                             countOutside += numDiscard
-                             print ("Discarded outside the domain:  ", countOutside)
+                            countOutside += numDiscard
+                            #print("Discarded outside the domain:  ", countOutside)
 
-                    if "merged locations" in row: 
-                        # Of the   100 observations reported,    80 of them are duplicates, leaving    20 merged locations
+                    if "merged locations" in row:
+                        # Of the   100 observations reported,
+                        #  80 of them are duplicates, leaving    20 merged locations
                         #print row
-                        tmp =row.split("Of the")[1]
+                        tmp = row.split("Of the")[1]
                         #print tmp
                         tmp2 = tmp.split(" observations reported,")
                         reported = tmp2[0]
@@ -103,76 +119,105 @@ for date in dates:
                         #print tmp3
                         tmp4 = tmp3[1].split(" merged locations")
                         merged = tmp4[0]
-                        
-                        #print "reported:  " ,reported,  "  .duplicates:  ", duplicates, ".merged:  ", merged
+
+                        #print "reported:  " ,reported,  "  .duplicates:  ",
+                        #       duplicates, ".merged:  ", merged
                         #break
                         countReported += int(reported)
                         countDuplicates += int(duplicates)
-                        countMerged += int(merged) 
-                        
-                print ("")
-                print ("")      
-                print ("Date:  " + date   + ".  Type:  " + type1)
-                print ("Failed Quality Control:   ", countQC)
-                print ("        MAX ERROR TEST:   ", errmxCount   )
-                print ("        BUDDY TEST:       ", buddyCount - 9  )
-                print ("Number of Observations Ingested:         ",countIngested)
-                print ("Number of Observations Empty:            ",countEmpty)
-                print ("Number of Observations Outside Domain:   ",countOutside)
-                print ("")
-                print ("Number of Observations Reported:     ",countReported)
-                print ("Number of Observations Duplicates:   ",countDuplicates)
-                print ("Number of Observations Merged:       ",countMerged)
-                print ("AVAILABLE IN WRF:  ",countMerged)
+                        countMerged += int(merged)
 
-                
-                print ("")                    
-                print ("")
+                print()
+                print()
+                print("Date:  " + date   + ".  Type:  " + type1)
+                print(shortfile)
+                print("Failed Quality Control:   ", countQC)
+                print("        MAX ERROR TEST:   ", errmxCount)
+                print("        BUDDY TEST:       ", buddyCount - 9)
+                # 9 is the number of "BUDDY" variables in the namelist file
+                print("Number of Observations Ingested:         ", countIngested)
+                print("Number of Observations Empty:            ", countEmpty)
+                print("Number of Observations Outside Domain:   ", countOutside)
+                print()
+                print("Number of Observations Reported:     ", countReported)
+                print("Number of Observations Duplicates:   ", countDuplicates)
+                print("Number of Observations Merged:       ", countMerged)
+                print("AVAILABLE IN WRF:  ", countMerged)
+                print()
+                print()
 
-                            
-            ## OBSGRID DATA FILES   
+
+            ## OBSGRID DATA FILES
             elif "OBS_DOMAIN" in shortfile:
-                infile = open(file,'rU')
+                if os.stat(file).st_size == 0:
+                    print("FILE IS EMPTY:"+ file)
+                    continue
+                infile = open(file, 'r')
                 readData = infile.readlines()
                 questionable = []
                 missingData = 0
                 countNoBuddy = 0
-                print ("AVAILABLE IN WRF:  ",  len(readData)/5)
+                print("AVAILABLE IN WRF:  ", len(readData)/5)
                 for row in readData:
                     row = row.rstrip("\n")
                     if "16384" in row:
-                       countNoBuddy +=1
-                       #print row
+                        countNoBuddy += 1
                     if "-888888.000 -888888.000" in row:
                         myRow = row.split()
                         questionable.append(myRow)
-                questDF = pd.DataFrame(questionable[:])  
-                shortDF = questDF.drop(questDF.columns[[2, 3, 12, 13,16, 17]], axis=1)
-                #print shortDF
-                #print len(questionable)
-                #numpy_matrix = shortDF.as_matrix()
+                questDF = pd.DataFrame(questionable[:])
+                shortDF = questDF.drop([2, 3, 12, 13, 16, 17], axis=1)
                 numpy_matrix = shortDF.values
 
                 for row2 in numpy_matrix:
                     if "-888888.000" in row2:
                         #print row2
-                        missingData +=1
-                print (shortfile + "   QC Flag because no buddy: " , countNoBuddy)
-                print (shortfile + "   Data potentially missing or partial: ", missingData)
-                
-            ## ALL WRF/WPS TEXT FILES    
-            elif "rsl.error.0000" in shortfile or ".out" in shortfile or ".log" in shortfile:
-                infile = open(file,'rU')
+                        missingData += 1
+                print(shortfile + "   QC Flag because no buddy: ", countNoBuddy)
+                print(shortfile + "   Data potentially missing or partial: ", missingData)
+
+            ## ALL WRF/WPS TEXT FILES
+            elif "rsl." in shortfile or ".out" in shortfile or ".log" in shortfile:
+                if "namelist.output" in shortfile:
+                    continue
+                #if "rsl.out." in shortfile and "0000" not in shortfile:
+                #    continue
+                infile = open(file, 'r')
                 readData = infile.readlines()
-                print (shortfile)
+
+                okay_string = "traj_opt is zero, but num_traj is not zero; setting num_traj to zero"
+
                 for row in readData:
-                    if "ERROR" in row or "error" in row or "Error" in row or "Warning" in row or "WARNING" in row:
-                        print ("CHECK?:  ", row)
-                    if "SUCCESS COMPLETE WRF" in row:
-                        print ("WRF RUN ON  " + date + "  FOR TYPE: " + type1 + " WAS SUCCESSFUL")
-                    
-        print ("")                    
-        print ("-------------------------END OF FILE---------------------------")             
-        print ("")
-        print ("")
-        print ("")
+                    if "ERROR" in row.upper():
+                        if okay_string in row:
+                            continue
+                        print("CHECK "+shortfile+" :  ", row.strip())
+                        num_errors += 1
+                    if "WARNING" in row.upper():
+                        if okay_string in row:
+                            continue
+                        print("CHECK "+shortfile+" :  ", row.strip())
+                        num_warnings += 1
+
+                    if "TBOUND" in row.upper():
+                        if okay_string in row:
+                            continue
+                        print("CHECK "+shortfile+" :  ", row.strip())
+                        num_tbounds += 1
+                    if "EXCEEDED CFL"  in row.upper():
+                        if okay_string in row:
+                            continue
+                        print("CHECK "+shortfile+" :  ", row.strip())
+                        num_cfl += 1
+                        # SHOULD BE CFL????
+                    if "SUCCESS COMPLETE WRF" in row and "rsl.out.0000" in shortfile:
+                        print("WRF RUN ON  " + date + "  FOR TYPE: " + type1 + " WAS SUCCESSFUL")
+        print("Errors: ", num_errors)
+        print("Warnings: ", num_warnings)
+        print("TBOUND: ", num_tbounds)
+        print("EXCEEDED CFL: ", num_cfl)
+        print()
+        print("-------------------------END OF FILE---------------------------")
+        print()
+        print()
+        print()
